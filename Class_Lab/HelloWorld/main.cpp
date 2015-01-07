f91fc2f40723a93eaf07b095cb46098c66863e45
ac1975334 <andrewcampbell1@live.com>
2015-01-06 20:03:04 -0800

    Signed-off-by: ac1975334 <andrewcampbell1@live.com>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Campbell
+ *
+ * Created on January 6, 2015, 6:05 PM
+ Purpose: First program
+ */
+//System Libraries 
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Functions Prototypes 
+
+//Execution Begins Here:
+
+int main(int argc, char** argv) {
+    //Output HelloWorld
+    cout<<"Hello World"<<endl;
+    //Exit
+    return 0;
+}
+
