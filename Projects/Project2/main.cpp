6e6240fdda9dcde70afac8d811c03ce573637fce
ac1975334 <andrewcampbell1@live.com>
2015-02-12 21:24:28 -0800

    Signed-off-by: ac1975334 <andrewcampbell1@live.com>

--- Projects/Project2/main.cpp
+++ Projects/Project2/main.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
 #include <iomanip>
 #include <fstream>
 
@@ -17,17 +16,19 @@ using namespace std;
 //User Libraries
 
 //Global constants
-const int COL=4;
+const int DIMNSN=4; //dimnsn x dimnsn for table size
+
 
 //Function prototypes
-int winBoard[4][COL] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
-void arySet(int [4][COL]);
-void prntBrd(int [4][COL] = winBoard);
-bool moveNum(int [4][COL], int &,int [4][COL], int &); //pass by reference
-bool gameWon(int [4][COL], int [4][COL]);
-int bot(int [4][COL],int [4][COL], int);
-bool winSpot (int, int [4][COL]);  //determines if the number is in winning spot
-int distance (int [4][COL], int [4][COL], int, int);
+
+void arySet(int [DIMNSN][DIMNSN]);
+void prntBrd(int [DIMNSN][DIMNSN]);
+bool moveNum(int [DIMNSN][DIMNSN], int &,int [DIMNSN][DIMNSN], int &); //pass by reference
+bool gameWon(int [DIMNSN][DIMNSN], int [DIMNSN][DIMNSN]);
+int bot(int [DIMNSN][DIMNSN],int [DIMNSN][DIMNSN], int);
+bool winSpot (int, int [DIMNSN][DIMNSN]);  //determines if the number is in winning spot
+int distance (int [DIMNSN][DIMNSN], int [DIMNSN][DIMNSN], int, int = 0);
+bool moveNumt(int [DIMNSN][DIMNSN], int &,int [DIMNSN][DIMNSN], int &);
 
 //Execution begins here:
 int main(int argc, char** argv) {
@@ -40,10 +41,11 @@ int main(int argc, char** argv) {
         
     bool moveOk = false;
     bool won = true;
-    int board[4][COL] = {0};
-    int oldBoard[4][COL] = {0};
+    char player = 'p';
+    int board[DIMNSN][DIMNSN] = {0};
+    int oldBoard[DIMNSN][DIMNSN] = {0};
     int move = 0,sChoice = 0;
-    int winBoard[4][COL] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+    int winBoard[DIMNSN][DIMNSN] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
     int prevMove = 0;
     unsigned int moves = 0;
     string line;
@@ -74,14 +76,18 @@ int main(int argc, char** argv) {
             cout << "Your board:" << endl;
             scores.close();
             break;
+        case 3: player = 'c';
+        break;
     }
     //create board
     arySet(board);
     //cout<< winSpot(12);
 
     do {
-        prntBrd(oldBoard);
+        //prntBrd(oldBoard);
         prntBrd(board);
+        if (player == 'p') {
+
         //do loop so if invalid move entered can enter another move
         do {
             cout<<"Enter the number you would like to have moved: "<<endl;
@@ -92,6 +98,11 @@ int main(int argc, char** argv) {
             moveOk = moveNum(board, move, oldBoard, prevMove);
             if (moveOk==false) cout<<"Number must be next to empty square."<<endl;
         }while (moveOk==false);
+        }
+        else {
+            move = bot(board,oldBoard,prevMove);
+            moveNum(board,move,oldBoard,prevMove);
+        }
         moves++;
         if (moves%100000==1){
             prntBrd(board);
@@ -110,7 +121,7 @@ int main(int argc, char** argv) {
 }
 
 //Creates a randomized array of numbers 1-15 and a 0 for the blank spot
-void arySet(int table[4][COL]){
+void arySet(int table[DIMNSN][DIMNSN]){
     for (int count = 0; count != 15; count++){
         int x = 0, y = 0;
         do{
@@ -123,7 +134,7 @@ void arySet(int table[4][COL]){
 }
 
 
-void prntBrd(int t[4][COL]){
+void prntBrd(int t[DIMNSN][DIMNSN]){
     for(int row = 0, rowPrnt = 0; row!=9; row++){
         if (row % 2 == 0){ //every other row (the ones without numbers))
                 cout<<"■■■■■■■■■■■■■■■■■■■■■■■";
@@ -142,7 +153,7 @@ void prntBrd(int t[4][COL]){
         cout<<endl;
     }
 }
-bool moveNum(int t[4][COL], int &moveNum, int oldt[4][COL], int &prevMove){
+bool moveNum(int t[DIMNSN][DIMNSN], int &moveNum, int oldt[DIMNSN][DIMNSN], int &prevMove){
     int xNum = 0, yNum = 0, xZero = 0, yZero = 0,temp = 0;
     //cout<<t [2][2]<<endl;
     
@@ -191,7 +202,7 @@ bool moveNum(int t[4][COL], int &moveNum, int oldt[4][COL], int &prevMove){
     
     return true;
 }
-bool gameWon(int t[4][COL], int win[4][COL]){
+bool gameWon(int t[DIMNSN][DIMNSN], int win[DIMNSN][DIMNSN]){
     for (int x = 0; x != 4; x++){
         for (int y = 0; y != 4; y++){
             if (t[x][y] != win[x][y]) return false;
@@ -199,7 +210,7 @@ bool gameWon(int t[4][COL], int win[4][COL]){
     }
     return true;
 }
-int bot (int t[4][COL],int oldt[4][COL], int prevMove) {
+int bot (int oldt[DIMNSN][DIMNSN],int t[DIMNSN][DIMNSN], int prevMove) {
     int x = 0, y = 0, toBeMoved = 0;
     int choiceNumbers [] = {};
     int borderNum [11] = {1,2,3,4,5,8,9,12,13,14,15};
@@ -215,10 +226,10 @@ int bot (int t[4][COL],int oldt[4][COL], int prevMove) {
         }
     }
     //checking if against box borders and not to try and move outside of it
-    if ((x > 0) && t[x-1][y] != prevMove)choiceNumbers[0] = t[x-1][y];
-    if ((x < 3) && t[x+1][y] != prevMove)choiceNumbers[1] = t[x+1][y];
-    if ((y > 0) && t[x][y-1] != prevMove)choiceNumbers[2] = t[x][y-1];
-    if ((y < 3) && t[x][y+1] != prevMove)choiceNumbers[3] = t[x][y+1];
+    if ((x > 0) && t[x-1][y] != prevMove) choiceNumbers[0] = t[x-1][y];
+    if ((x < 3) && t[x+1][y] != prevMove) choiceNumbers[1] = t[x+1][y];
+    if ((y > 0) && t[x][y-1] != prevMove) choiceNumbers[2] = t[x][y-1];
+    if ((y < 3) && t[x][y+1] != prevMove) choiceNumbers[3] = t[x][y+1];
     
     int choic;
     if (0<((distance(t,oldt,choiceNumbers[0],prevMove))<choic)) choic=0;
@@ -241,8 +252,8 @@ int bot (int t[4][COL],int oldt[4][COL], int prevMove) {
 
 
 //used to figure out if number is in it's winning spot
-bool winSpot (int n,int tbl[4][COL]) {
-    int winBoard[4][COL] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0}, y=n;  
+bool winSpot (int n,int tbl[DIMNSN][DIMNSN]) {
+    int winBoard[DIMNSN][DIMNSN] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0}, y=n;  
     if (n==0)return false;
     if ((n % 4)==0)y-=1; 
    // cout<<n<<" vs "<<(tbl[(n / 4)][(n % 4) - 1])<<endl;
@@ -253,9 +264,9 @@ bool winSpot (int n,int tbl[4][COL]) {
     return false;
 }
 
-int distance (int t[4][COL], int oldt[4][COL], int move, int prevMove){ //trying to do manhattan distance
+int distance (int oldt[DIMNSN][DIMNSN], int t[DIMNSN][DIMNSN], int move, int prevMove){ //trying to do manhattan distance
     if (move==0) return 0;
-    moveNum(t, move, oldt, prevMove);
+    moveNumt(t, move, oldt, prevMove);
     int dist=0;
     int arAdd = 0;
     int count = 0;
@@ -272,18 +283,22 @@ int distance (int t[4][COL], int oldt[4][COL], int move, int prevMove){ //trying
             dist+=count;
         }
     }
-    moveNum(t, prevMove, oldt, move); //returning table to original form
+    //moveNum(t, prevMove, oldt, move); //returning table to original form
     return dist;
 }
-bool moveNumt(int t[4][COL], int &moveNum, int oldt[4][COL], int &prevMove){
+bool moveNumt(int t[DIMNSN][DIMNSN], int &moveNum, int oldt[DIMNSN][DIMNSN], int &prevMove){
     int xNum = 0, yNum = 0, xZero = 0, yZero = 0,temp = 0;
     //cout<<t [2][2]<<endl;
+    //copying previous turn's board into another array
+    for (int i = 0; i < 4; i++){
+        for (int y = 0; y < 4; y++){
+            oldt[i][y] = t[i][y];
     
     //keep previous number from being played again
     //if (prevMove == moveNum) return false;
-    while (t[xNum][yNum] != moveNum) {
+    while (oldt[xNum][yNum] != moveNum) {
         for (int i = 0; i != 4; i++) {
-            if (t[xNum][i] == moveNum) {
+            if (oldt[xNum][i] == moveNum) {
                 yNum = i;
                 xNum--;
             }
@@ -291,35 +306,32 @@ bool moveNumt(int t[4][COL], int &moveNum, int oldt[4][COL], int &prevMove){
         xNum++;
     }
     //checking if valid move, as in 0 left/right or up/down to it
-    if (xNum - 1 >= 0 && t[xNum-1][yNum] == 0) {
+    if (xNum - 1 >= 0 && oldt[xNum-1][yNum] == 0) {
         xZero = xNum - 1;
         yZero = yNum;
     }
-    else if (xNum + 1 <= 3 && t[xNum+1][yNum] == 0) {
+    else if (xNum + 1 <= 3 && oldt[xNum+1][yNum] == 0) {
         xZero = xNum + 1;
         yZero = yNum;
     }
-    else if (yNum - 1 >= 0 && t[xNum][yNum-1] == 0) {
+    else if (yNum - 1 >= 0 && oldt[xNum][yNum-1] == 0) {
         xZero = xNum;
         yZero = yNum - 1;
     }
-    else if (yNum + 1 <= 3 && t[xNum][yNum+1] == 0) {
+    else if (yNum + 1 <= 3 && oldt[xNum][yNum+1] == 0) {
         xZero = xNum;
         yZero = yNum + 1;
     }
     else return false;
     
-    //copying previous turn's board into another array
-    for (int i = 0; i < 4; i++){
-        for (int y = 0; y < 4; y++){
-            oldt[i][y] = t[i][y];
+
     }
     }
   
     //cout<<xNum<<"  "<<yNum<<endl;
     //move number positions
-    t[xNum][yNum] = 0;
-    t[xZero][yZero] = moveNum;
+    oldt[xNum][yNum] = 0;
+    oldt[xZero][yZero] = moveNum;
     prevMove = moveNum;
     
     return true;
